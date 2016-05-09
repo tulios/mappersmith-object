541c90663164a672aaf12d3f5c4b25e1c5fa8547
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-05-09 16:44:04 +0200

    updated pre-build scripts

--- build/mappersmith-object.js
+++ build/mappersmith-object.js
@@ -1,6 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.MappersmithObject = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
- * MappersmithObject 0.1.1
+ * MappersmithObject 0.2.0
  * https://github.com/tulios/mappersmith-object
  */
 var Instance = require('./src/instance');
@@ -184,7 +184,7 @@ Instance.prototype = {
   },
 
   toArray: function(stringChain) {
-    var value = this.get(stringChain);
+    var value = stringChain ? this.get(stringChain) : this.attributes();
     if (!isDefined(value)) return [];
     var objToString = Object.prototype.toString.call(value);
     return objToString === '[object Array]' ? value : [value];
@@ -212,6 +212,9 @@ Instance.prototype.extend = function(mixin) {
   }.bind(this));
   return this;
 }
+
+// Alias
+Instance.prototype.is = Instance.prototype.has;
 
 module.exports = Instance;
 
